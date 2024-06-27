008f426fe9ad30d90d8f4634ca9b97c71289a68d
jondeveloper <jondeveloper0@gmail.com>
2024-06-26 22:13:14 -0400

    Successfully connected to remote mongoDB. I added usename and password to my env to keep them hidden and private

--- database.py
+++ database.py
@@ -1,4 +1,20 @@
-from pymongo import MongoClient
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
 
-client = MongoClient("localhost",27017)
+load_dotenv()
+MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 
+uri = "mongodb+srv://" + MONGO_USERNAME +":" + MONGO_PASSWORD + "@cluster0.jxpmcwi.mongodb.net/?appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
