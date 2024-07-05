92e91f36e0197bdc14d37a824c8580ae05423d30
Trust-Worthy <jondeveloper0@gmail.com>
2024-07-05 10:38:48 -0400

    Removing the global variables.

--- MongoConnection.py
+++ MongoConnection.py
@@ -9,34 +9,61 @@ import Police_Activity_Scraper as PAS
 Global Variables
 """
 COUNT = 0
+MONGO_USERNAME = ""
+MONGO_PASSWORD = ""
 
-load_dotenv()
-MONGO_USERNAME = os.getenv("MONGO_USERNAME")
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 
-uri = "mongodb+srv://" + MONGO_USERNAME +":" + MONGO_PASSWORD + "@cluster0.jxpmcwi.mongodb.net/?appName=Cluster0"
-MONGO_CLIENT = MongoClient(uri, server_api=ServerApi('1'))
-# yt-database is the database that I'll be storing the information from the yt-vids in
-DATABASE = MONGO_CLIENT['yt-database']
-UOF_COLLECTION = DATABASE['yt-force-used-vids'] # use of force
-NUOF_COLLECTION = DATABASE['yt-no-force-vids'] # non-use of force
 
-def confirm_connection(client):
+
+
+
+def get_mongo_creds():
+    global MONGO_USERNAME
+    global MONGO_PASSWORD
+
+    load_dotenv()
+    MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+    MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+
+def construct_mongo_connection():
+    uri = "mongodb+srv://" + MONGO_USERNAME +":" + MONGO_PASSWORD + "@cluster0.jxpmcwi.mongodb.net/?appName=Cluster0"
+    mongo_client = MongoClient(uri, server_api=ServerApi('1'))
+    
+
+    test_mongo_connection(mongo_client)
+
+def create_establish_yt_database():
+    global YT_DATABASE
+
+    if 'yt-database' in MONGO_CLIENT.list_database_names():
+        YT_DATABASE = MONGO_CLIENT['yt-database']
+    else:
+        print("yt-database does not exist")
+
+def instantiate_UOF_collection():
+    global UOF_COLLECTION
+    
+    UOF_COLLECTION = YT_DATABASE['yt-force-used-vids']
+def instantiate_NUOF_database():
+    global NUOF_COLLECTION
+
+    NUOF_COLLECTION = YT_DATABASE['yt-no-force-vids']
+
+def test_mongo_connection(mongo_client):
 
     # Send a ping to confirm a successful connection
     try:
-        client.admin.command('ping')
+        mongo_client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
         print(e)
 
-    for db_name in client.list_database_names():
+    for db_name in mongo_client.list_database_names():
         print(db_name)
-
-
 
 def append_UOF_COLLECTION(dict_response):
     global COUNT
+    global UOF_COLLECTION
     """
     This function places takes the fields from the dictionary responses from Police Activity Scraper and inputs them into a mongoDB database
     according to the correct fields.
@@ -107,8 +134,21 @@ def append_UOF_COLLECTION(dict_response):
     
     COUNT+=1
 
-if __name__ == "__main__":
-    confirm_connection()
-    append_UOF_COLLECTION()
 
+def main():
+    get_mongo_creds()
+    
+
+    construct_mongo_connection()
+
+    #create_establish_yt_database()
+
+    #instantiate_UOF_collection()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
 
