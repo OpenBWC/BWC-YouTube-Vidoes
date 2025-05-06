c2b47e23e8e8ee15b9c9d2349f3b03b488d51408
Trust-Worthy <jondeveloper0@gmail.com>
2025-05-06 16:16:17 -0400

    Organizing code.

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-fields = ['Word','Occurances']
-    
-with open("tokens.csv",'w') as tokens_csv:
-    writer = csv.DictWriter(tokens_csv,fieldnames=fields)
-
-    writer.writeheader()
-
-    writer.writerows(TOKENS_DICTIONARY)
